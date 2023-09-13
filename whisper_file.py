98efb8499140b5ebfd951126a120a75ff6b5bdee
Maxens Telle <tmax.2017.03@gmail.com>
2023-09-13 13:54:11 +0200

    update whisper avec openai

--- whisper_file.py
+++ whisper_file.py
@@ -1,7 +1,13 @@
-import whisper
+#import whisper # remote or local transcription options
+import openai
+import os
+
+openai.api_key = "Key-API" # replace "Key-API" with your api key 
 
 def translate_in_chat():
-    model = whisper.load_model("medium") #Call the model whisper
-    result = model.transcribe("vocal0.wav") #audio transcription
-    return result["text"]
+    #model = whisper.load_model("medium") # Call the model whisper
+    #result = model.transcribe("vocal0.wav") # audio transcription
+    audio_file = open("vocal0.wav", "rb") # open audio
+    transcript = openai.Audio.transcribe("whisper-1", audio_file) # audio transcription
+    return transcript["text"]
 # end translate_in_chat
