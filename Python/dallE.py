912f8a2657248d4f9cfff583d8de990b4dff5a4d
Naxem <tmax.2017.03@gmail.com>
2024-03-20 12:24:15 +0100

    update 20/03/2024
    update openai 0.28

--- Python/dallE.py
+++ Python/dallE.py
@@ -4,11 +4,10 @@ import apiKey
 openai.api_key = apiKey.key #replace "Key-API" with your api key in file (apiKey)
 
 def dallE(txt):
-    response = openai.images.generate(
+    response = openai.Image.create(
         model="dall-e-3",
         prompt=txt,
         size="1024x1024",
-        quality="hd",
         n=1,
     )
 
